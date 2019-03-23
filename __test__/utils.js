be3dadb7cb7005ff63ad48ff0a8446004de0dea4
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-23 00:54:42 -0300

    fix selector

--- __test__/utils.js
+++ __test__/utils.js
@@ -1,3 +1,3 @@
 export const findByTestAttribute = (shallowWrapper, testAttributeName) => {
-  return shallowWrapper.find(`[data-test="${testAttributeName}"`);
+  return shallowWrapper.find(`[data-test="${testAttributeName}"]`);
 }
