835b70c866812ac12ec062a10fefa01aaf829af1
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-27 21:11:28 -0300

    add 'shouldnt throw warning message with expected props' text and set the default props

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import Congrats from './';
-import { findByTestAttribute } from '../../../__test__/utils';
+import { findByTestAttribute, checkProps } from '../../../__test__/utils';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 function createShallowWrapper(props = {}, state = null) {
-  const wrapper = shallow(<Congrats {...props} />);
+  const defaultProps = { success: false };
+  const mergedProps = { ...defaultProps, ...props };
+  const wrapper = shallow(<Congrats {...mergedProps} />);
   if (state) wrapper.setState(state);
   return wrapper;
 }
@@ -28,4 +30,10 @@ it('should render non-empty congrats message when `success` prop is true', () =>
   const wrapper = createShallowWrapper({ success: true });
   const congratsMessage = findByTestAttribute(wrapper, 'congrats-message');
   expect(congratsMessage.text().length).not.toBe(0);
+});
+
+it('shouldn\'t throw warning message with expected props', () => {
+  const expectedProps = { success: false };
+  const propsErros = checkProps(Congrats, expectedProps);
+  expect(propsErros).toBeUndefined();
 });
