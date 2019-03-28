9c13c89e2cc9c8c59585b94f5b18660e1fea581e
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-27 21:46:35 -0300

    add setupTest file

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Congrats from './';
 import { findByTestAttribute, checkProps } from '../../../__test__/utils';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 function createShallowWrapper(props = {}, state = null) {
   const defaultProps = { success: false };
