5c2608d9b4d391dfc9d5f791560f62b876ead0f4
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-27 21:04:09 -0300

    add checkProps functionality

--- __test__/utils.js
+++ __test__/utils.js
@@ -1,3 +1,15 @@
+import checkPropTypes from 'check-prop-types';
+
 export const findByTestAttribute = (shallowWrapper, testAttributeName) => {
   return shallowWrapper.find(`[data-test="${testAttributeName}"]`);
-}
+};
+
+export const checkProps = (componentToCheckTheProps, expectedProps) => {
+  const propsErros = checkPropTypes(
+    componentToCheckTheProps.propTypes,
+    expectedProps,
+    'prop',
+    componentToCheckTheProps.name
+  );
+  return propsErros;
+};
