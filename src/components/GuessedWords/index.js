5afec07cbec16e3cdc66d52263c0b8e33f51c7f1
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-04-06 17:56:48 -0300

    add redux

--- src/components/GuessedWords/index.js
+++ src/components/GuessedWords/index.js
@@ -22,12 +22,10 @@ function GuessedWords({ guessedWords }) {
     return (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr>
               <th>Guess</th>
-            </tr>
-            <tr>
               <th>Matching Letters</th>
             </tr>
           </thead>
